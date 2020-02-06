d5781260b36433e6201d2160cd987412b6a97f2c
Roberta Munive <ramunive@gmail.com>
2020-02-06 20:28:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 var recipes = {flour: "1 cup", eggs: 3};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return newObj;
+}
